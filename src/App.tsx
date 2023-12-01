2301c7538a594362b285743a70770b3c8ae3e003
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-12-01 17:18:43 +0100

    delete profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,42 @@
-import { cloneElement } from "react";
-import { useLocation, useRoutes } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import {cloneElement} from "react";
+import {useLocation, useRoutes} from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
 import ToasterComponent from "./Components/ToasterComponent";
 import Header from "./Layout/Header";
 import Login from "./Pages/Auth/Login";
 import Register from "./Pages/Auth/Register";
 import Home from "./Pages/Home/Home";
-import Profile from "./Pages/Profile/Profile";
-import Settings from "./Pages/Profile/Settings";
 
 export default function App() {
-  const element = useRoutes([
-    {
-      path: "/",
-      element: <Header />,
-      children: [
+    const element = useRoutes([
         {
-          path: "/",
-          element: <Home />,
+            path: "/",
+            element: <Header/>,
+            children: [
+                {
+                    path: "/",
+                    element: <Home/>,
+                },
+            ],
         },
         {
-          path: "/profile/:userId",
-          element: <Profile />,
+            path: "/login",
+            element: <Login/>,
         },
         {
-          path: "/settings",
-          element: <Settings />,
+            path: "/register",
+            element: <Register/>,
         },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-  ]);
+    ]);
 
-  const location = useLocation();
+    const location = useLocation();
 
-  if (!element) return null;
+    if (!element) return null;
 
-  return (
-    <AnimatePresence mode="wait" initial={false}>
-      <ToasterComponent />
-      {cloneElement(element, { key: location.pathname })}
-    </AnimatePresence>
-  );
+    return (
+        <AnimatePresence mode="wait" initial={false}>
+            <ToasterComponent/>
+            {cloneElement(element, {key: location.pathname})}
+        </AnimatePresence>
+    );
 }
