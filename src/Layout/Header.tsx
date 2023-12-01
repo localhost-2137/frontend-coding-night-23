042be6b5586f5e5f92f75355c21635a0dd7d3bf2
Majkipl27 <tomaszmamala@gmail.com>
2023-12-01 17:58:30 +0100

    Added logo

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -3,6 +3,7 @@ import { useAtomValue } from "jotai";
 import { userAtom } from "../Atoms";
 import toast from "react-hot-toast";
 import Nav from "../Components/Nav";
+import logo from "./Assets/logo.png";
 
 export default function Header() {
   const user = useAtomValue(userAtom);
@@ -26,8 +27,9 @@ export default function Header() {
 
   return (
     <>
-      <header className="w-screen border-b-2 border-amber-600 flex justify-between items-center sm:px-8 px-4 py-2">
-        <Link to="/" className="flex items-center w-fit">
+      <header className="w-screen border-b-2 border-amber-600 flex justify-between items-center sm:px-8 px-4 py-4">
+        <Link to="/" className="flex items-center w-fit gap-4">
+          <img src={logo} alt="" className="w-12" />
           <h1 className="roboto text-3xl">Home Lord</h1>
         </Link>
         <Nav user={user} logout={logout} />
